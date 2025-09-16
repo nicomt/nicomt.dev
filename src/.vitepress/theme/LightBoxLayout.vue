2c5b82919f88f7179e859d56df886d6c55c25ea1
Nicolas Martinez <17040442+nicomt@users.noreply.github.com>
2025-09-16 12:30:02 +0000

    Lightbox fixes: Fixing lightbox init on route change

--- src/.vitepress/theme/LightBoxLayout.vue
+++ src/.vitepress/theme/LightBoxLayout.vue
@@ -7,39 +7,38 @@ import PhotoSwipeLightbox from 'photoswipe/lightbox';
 import 'photoswipe/style.css';
 
 const { Layout } = DefaultTheme;
+let lightbox: PhotoSwipeLightbox | null = null;
 const router = useRouter();
 
-let lightbox: PhotoSwipeLightbox | null = null;
-
 const initLightbox = () => {
+  if (!lightbox) {
+    lightbox = new PhotoSwipeLightbox({
+      gallery: '.gallery-page p:has(img)',
+      children: 'a',
+      pswpModule: () => import('photoswipe'),
+    });
+    lightbox.init();
+  }
+};
+
+const destroyLightbox = () => {
   if (lightbox) {
     lightbox.destroy();
     lightbox = null;
   }
-  lightbox = new PhotoSwipeLightbox({
-    gallery: '.gallery-page p:has(img)',
-    children: 'a',
-    pswpModule: () => import('photoswipe')
-  });
-  lightbox.init();
+};
+
+router.onAfterRouteChanged = () => {
+  destroyLightbox();
+  initLightbox();
 };
 
 onMounted(() => {
   initLightbox();
-  router.onAfterRouteChanged = () => {
-    if (lightbox) {
-      lightbox.refresh();
-    } else {
-      initLightbox();
-    }
-  };
 });
 
 onUnmounted(() => {
-  if (lightbox) {
-    lightbox.destroy();
-    lightbox = null;
-  }
+  destroyLightbox();
 });
 </script>
 
@@ -55,15 +54,5 @@ onUnmounted(() => {
 .medium-zoom-overlay,
 .medium-zoom-image--opened {
   z-index: 999;
-}
-
-/* Ensure PhotoSwipe overlays appear above site chrome */
-.pswp {
-  z-index: 9999;
-}
-
-/* Make sure large images scale to fit viewport */
-.pswp__img {
-  object-fit: contain;
 }
 </style>
